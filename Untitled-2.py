fba2a964026cfae14b7522a3fa26d9fcfba512dd
Sandilem10 <matsendzingeko@gmail.com>
2025-03-31 22:18:36 +0200

    Initial commit

---
+++ Untitled-2.py
@@ -0,0 +1,24 @@
+
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)
+
+my_list.extend([50, 60, 70])
+
+my_list.pop()
+
+my_list.sort()
+
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
+
+print("Final list:", my_list)
+# Output: Final list: [10, 15, 20, 30, 40, 50, 60]
+# The list is sorted and the last element (70) is removed.
+# The index of 30 is printed.
+# The final list is printed.
